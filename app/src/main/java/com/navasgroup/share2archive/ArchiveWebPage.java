9984f0f0a956664b11701e0e919695b9c96e6456
John Navas <jn2@navasgroup.com>
2016-11-15 07:31:51 -0800

    Version 1.20

--- app/src/main/java/com/navasgroup/share2archive/ArchiveWebPage.java
+++ app/src/main/java/com/navasgroup/share2archive/ArchiveWebPage.java
@@ -4,21 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
-import static java.lang.Math.max;
-
 public class ArchiveWebPage extends Activity {
+    private static final String TAG = "Share2Archive";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String sharedText = getIntent().getStringExtra(Intent.EXTRA_TEXT);  // get URL
+        Log.i(TAG, "EXTRA_TEXT: \"" + sharedText + "\"");
         if (sharedText != null) {
             Uri url = Uri.parse("https://archive.is/?run=1&url=" + getURL(sharedText));
             Intent intent = new Intent(Intent.ACTION_VIEW, url);
             if (intent.resolveActivity(getPackageManager()) != null) {
                 startActivity(intent);
             } else {
+                Log.w(TAG, "Failed to start web browser activity!");
                 Toast.makeText(this, "Please install a web browser",  Toast.LENGTH_LONG).show();
             }
         }
@@ -32,6 +35,8 @@ public class ArchiveWebPage extends Activity {
             n = source.indexOf("https://");     // else find HTTPS
         }
         String url = (n > 0) ? source.substring(n) : source ;       // strip anything before URL
-        return url.split("\\s",2)[0];   // strip any trailing material starting with whitespace
+        url = url.split("\\s",2)[0];   // strip any trailing material starting with whitespace
+        Log.d(TAG, "URL: \"" + url + "\"");
+        return url;
     }
 }
