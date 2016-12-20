e998acd0752f11da5a1d2575ac2703c137735a84
John Navas <jn2@navasgroup.com>
2016-12-20 07:37:38 -0800

    Version 1.70

--- app/src/main/java/com/navasgroup/share2archive/ArchiveWebPage.java
+++ app/src/main/java/com/navasgroup/share2archive/ArchiveWebPage.java
@@ -8,22 +8,20 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class ArchiveWebPage extends Activity {
-    private static final String TAG = "Share2Archive";
-    // archive.today works around possible blocking of archive.is
-    private static final String PRE = "https://archive.today/?run=1&url=";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String tag = getString(R.string.app_name);							// for logging
+        String pre = getString(R.string.prepend);							// prepend the URL
         String sharedText = getIntent().getStringExtra(Intent.EXTRA_TEXT);  // get URL
-        Log.i(TAG, "EXTRA_TEXT: \"" + sharedText + "\"");
+        Log.i(tag, "EXTRA_TEXT: \"" + sharedText + "\"");
         if (sharedText != null) {
-            Uri url = Uri.parse(PRE + getURL(sharedText));
+            Uri url = Uri.parse(pre + getURL(sharedText));
             Intent intent = new Intent(Intent.ACTION_VIEW, url);
             if (intent.resolveActivity(getPackageManager()) != null) {
                 startActivity(intent);
             } else {
-                Log.w(TAG, "Failed to start web browser activity!");
+                Log.w(tag, "Failed to start web browser activity!");
                 Toast.makeText(this, "Please install a web browser",  Toast.LENGTH_LONG).show();
             }
         }
@@ -33,15 +31,16 @@ public class ArchiveWebPage extends Activity {
     // extract URL to handle bad browsers that pad URL before and/or after; e.g.,
     // "IBM - United States http://www.ibm.com/us-en/	(Share from CM Browser)"
     String getURL(String source) {
-        String url = source.toLowerCase();      // lower case for scan
-        int r = url.indexOf("http://");         // find HTTP
-        int s = url.indexOf("https://");        // find HTTPS
+        String tag = getString(R.string.app_name);				// for logging
+        String url = source.toLowerCase();      				// lower case for scan
+        int r = url.indexOf("http://");         				// find HTTP
+        int s = url.indexOf("https://");        				// find HTTPS
         int n = Math.min((r < 0 ? s : r), (s < 0 ? r : s));		// take first http or https
         url = (n > 0) ? source.substring(n) : source ;			// strip anything before URL
         url = url.split("\\s",2)[0];   // strip any trailing material starting with whitespace
-        Log.d(TAG, "URL: \"" + url + "\"");
-        url = Uri.encode(url);                  // encode URL
-        Log.d(TAG, "Encoded: \"" + url + "\"");
+        Log.d(tag, "URL: \"" + url + "\"");
+        url = Uri.encode(url, "://?=&");						// encode URL
+        Log.d(tag, "Encoded: \"" + url + "\"");
         return url;
     }
 }
